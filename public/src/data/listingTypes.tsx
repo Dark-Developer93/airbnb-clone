89029a5ba9adfe6b69a7281d0ed935a42c618a44
Abdullah Sadaqa <eng.abdullahhasan2016@gmail.com>
2025-01-02 23:56:58 +0200

    Convert amenities and listing types components to TypeScript; remove old listing types file

---
+++ public/src/data/listingTypes.tsx
@@ -0,0 +1,65 @@
+import Barn from "../svg/lisitngTypes/barn.svg";
+import BedBreakfast from "../svg/lisitngTypes/bed-breakfast.svg";
+import Boat from "../svg/lisitngTypes/boat.svg";
+import Cabin from "../svg/lisitngTypes/cabin.svg";
+import Campervan from "../svg/lisitngTypes/campervan.svg";
+import CasaParticular from "../svg/lisitngTypes/casa-particular.svg";
+import Castle from "../svg/lisitngTypes/castle.svg";
+import Cave from "../svg/lisitngTypes/cave.svg";
+import Container from "../svg/lisitngTypes/container.svg";
+import CycladicHome from "../svg/lisitngTypes/cycladic-home.svg";
+import Dammuso from "../svg/lisitngTypes/dammuso.svg";
+import Dome from "../svg/lisitngTypes/dome.svg";
+import EarthHome from "../svg/lisitngTypes/earth-home.svg";
+import Farm from "../svg/lisitngTypes/farm.svg";
+import Flat from "../svg/lisitngTypes/flat.svg";
+import GuestHouse from "../svg/lisitngTypes/guest-house.svg";
+import Hotel from "../svg/lisitngTypes/hotel.svg";
+import House from "../svg/lisitngTypes/house.svg";
+import Houseboat from "../svg/lisitngTypes/houseboat.svg";
+import Kezhan from "../svg/lisitngTypes/kezhan.svg";
+import Minsu from "../svg/lisitngTypes/minsu.svg";
+import Riad from "../svg/lisitngTypes/riad.svg";
+import Ryokan from "../svg/lisitngTypes/ryokan.svg";
+import ShepherdHut from "../svg/lisitngTypes/shepherd-hut.svg";
+import Tent from "../svg/lisitngTypes/tent.svg";
+import TinyHome from "../svg/lisitngTypes/tiny-home.svg";
+import Tower from "../svg/lisitngTypes/tower.svg";
+import TreeHouse from "../svg/lisitngTypes/tree-house.svg";
+import Trullo from "../svg/lisitngTypes/trullo.svg";
+import Windmill from "../svg/lisitngTypes/windmill.svg";
+import Yurt from "../svg/lisitngTypes/yurt.svg";
+
+export const listingTypes = [
+  { name: "House", svgPath: <House /> },
+  { name: "Flat/apartment", svgPath: <Flat /> },
+  { name: "Barn", svgPath: <Barn /> },
+  { name: "Bed & breakfast", svgPath: <BedBreakfast /> },
+  { name: "Boat", svgPath: <Boat /> },
+  { name: "Cabin", svgPath: <Cabin /> },
+  { name: "Campervan/motorhouse", svgPath: <Campervan /> },
+  { name: "Casa particular", svgPath: <CasaParticular /> },
+  { name: "Castle", svgPath: <Castle /> },
+  { name: "Cave", svgPath: <Cave /> },
+  { name: "Container", svgPath: <Container /> },
+  { name: "Cycladic home", svgPath: <CycladicHome /> },
+  { name: "Dammuso", svgPath: <Dammuso /> },
+  { name: "Dome", svgPath: <Dome /> },
+  { name: "Earth home", svgPath: <EarthHome /> },
+  { name: "Farm", svgPath: <Farm /> },
+  { name: "Guest house", svgPath: <GuestHouse /> },
+  { name: "Hotel", svgPath: <Hotel /> },
+  { name: "Houseboat", svgPath: <Houseboat /> },
+  { name: "Kezhan", svgPath: <Kezhan /> },
+  { name: "Minsu", svgPath: <Minsu /> },
+  { name: "Riad", svgPath: <Riad /> },
+  { name: "Ryokan", svgPath: <Ryokan /> },
+  { name: "Shepherd's hut", svgPath: <ShepherdHut /> },
+  { name: "Tent", svgPath: <Tent /> },
+  { name: "Tiny home", svgPath: <TinyHome /> },
+  { name: "Tower", svgPath: <Tower /> },
+  { name: "Tree house", svgPath: <TreeHouse /> },
+  { name: "Trullo", svgPath: <Trullo /> },
+  { name: "Windmill", svgPath: <Windmill /> },
+  { name: "Yurt", svgPath: <Yurt /> },
+];
